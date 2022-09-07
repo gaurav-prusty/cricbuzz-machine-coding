3404cca229d78ea4da99b582b776a6cf2af25598
gaurav-prusty <prustycolab@gmail.com>
2022-09-07 20:46:33 +0530

    feat: side-effects
    These Include:
    - On submit, highlight the word if it exists
    - Simultaneously, gray out and strike-through the corresponding word in the  left-pane.
    - if word entered by user does not exist in the set, a 3-second message is shown

--- src/components/TableDataItem.js
+++ src/components/TableDataItem.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { indexToHighlight } from '../App';
 
 function TableDataItem(props) {
-
-
     function decideHighlight() {
-        // indexToHighlight.map(index=> {
-        //     if(index[0] !== props.row && index[1] !== props.column) return false
-        //     return true;
-        // })
         if(!JSON.stringify(indexToHighlight).includes(JSON.stringify([props.row, props.column]))) return false
         return true;
     }
-    
+
     return (
-        <td style={{background: decideHighlight() ? 'green' : 'white'}}>
+        <td style={{background: decideHighlight() ? 'lightgreen' : 'white'}}>
             {props.content}
         </td>
     );
